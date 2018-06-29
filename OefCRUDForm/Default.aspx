e2de89aa94033adcbe74e88f795bcd0d6652780d
Tom Tilley <TomT@cursisten.intecbrussel.intra>
2018-06-29 11:35:48 +0200

    InitialCommit

---
+++ OefCRUDForm/Default.aspx
@@ -0,0 +1,100 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="OefCRUDForm.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+    <style>
+        td, th{
+            padding: 5px;
+        }
+    </style>
+    <title></title>
+</head>
+<body>
+    <div class="container">
+        <form id="form1" runat="server" class="form-horizontal">
+            <div class="page-header">
+                <h1>Oefening CRUD-form</h1>
+            </div>
+            <div class="form-group" style="padding-top: 3em;">
+                <label class="control-label col-sm-2" for="txtName">Name:</label>
+                <div class="col-sm-3">
+                    <asp:TextBox ID="txtName" runat="server"  class="form-control" placeholder="Enter name..."></asp:TextBox>
+                </div>
+                <div class="col-sm-1">
+                    <asp:RequiredFieldValidator ID="ReqName" runat="server" ErrorMessage="Name is required field" ControlToValidate="txtName"><i class="fa fa-exclamation-circle"></i></asp:RequiredFieldValidator>
+                </div>
+                <div class="col-sm-2">
+                    <asp:DropDownList ID="ddlCommands" runat="server" CssClass="form-control" AutoPostBack="True" OnSelectedIndexChanged="ddlCommands_SelectedIndexChanged" TabIndex="1"></asp:DropDownList>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="txtAge">Age:</label>
+                <div class="col-sm-3">
+                    <asp:TextBox ID="txtAge" runat="server"  class="form-control" placeholder="Enter age..." TextMode="Number"></asp:TextBox>
+                </div>
+                <div class="col-sm-1">
+                    <asp:RequiredFieldValidator ID="ReqAge" runat="server" ErrorMessage="Age is required field" ControlToValidate="txtAge"><i class="fa fa-exclamation-circle"></i></asp:RequiredFieldValidator>
+                </div>
+                <div class="col-sm-2">
+                    <asp:TextBox ID="txtID" runat="server"  class="form-control" placeholder="Enter ID..." Visible="False" TabIndex="1"></asp:TextBox>
+                </div>
+                <div class="col-sm-2">
+                    <asp:RequiredFieldValidator ID="ReqID" runat="server" ErrorMessage="ID is required" ControlToValidate="txtID" Enabled="False"><i class="fa fa-exclamation-circle"></i></asp:RequiredFieldValidator>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="txtAddress">Address:</label>
+                <div class="col-sm-3">
+                    <asp:TextBox ID="txtAddress" runat="server"  class="form-control" placeholder="Enter address..."></asp:TextBox>
+                </div>
+                <div class="col-sm-1">
+                    <asp:RequiredFieldValidator ID="ReqAddress" runat="server" ErrorMessage="Address is required field" ControlToValidate="txtAddress"><i class="fa fa-exclamation-circle"></i></asp:RequiredFieldValidator>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="control-label col-sm-2" for="txtCity">City:</label>
+                <div class="col-sm-3">
+                    <asp:TextBox ID="txtCity" runat="server"  class="form-control" placeholder="Enter city..."></asp:TextBox>
+                </div>
+                <div class="col-sm-1">
+                    <asp:RequiredFieldValidator ID="ReqCity" runat="server" ErrorMessage="City is required field" ControlToValidate="txtCity"><i class="fa fa-exclamation-circle"></i></asp:RequiredFieldValidator>
+                </div>
+                <div class="col-sm-2">
+                    <asp:Button ID="btnExecute" runat="server" Text="Execute" CssClass="btn btn-primary" OnClick="btnExecute_Click"/>
+                </div>
+            </div>
+            <div class="row"  style="padding-top: 2em; padding-bottom: 2em;">
+                <div class ="col-sm-8 col-sm-offset-2">
+                    <asp:Label ID="lblLog" runat="server" Text=""></asp:Label></div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-8 col-sm-offset-2">
+                    <asp:GridView ID="GridView1" runat="server" CellPadding="6" ForeColor="#333333" GridLines="None">
+                        <AlternatingRowStyle BackColor="White" />
+                        <EditRowStyle BackColor="#999999" />
+                        <FooterStyle BackColor="#337ab7" Font-Bold="True" ForeColor="White" />
+                        <HeaderStyle BackColor="#337ab7" Font-Bold="True" ForeColor="White" />
+                        <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />
+                        <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />
+                        <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />
+                        <SortedAscendingCellStyle BackColor="#E9E7E2" />
+                        <SortedAscendingHeaderStyle BackColor="#506C8C" />
+                        <SortedDescendingCellStyle BackColor="#FFFDF8" />
+                        <SortedDescendingHeaderStyle BackColor="#6F8DAE" />
+                    </asp:GridView>
+                </div>
+            </div>
+        </form>
+    </div>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</body>
+</html>
---
+++ OefCRUDForm/Default.aspx.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+namespace OefCRUDForm
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                FillDDL();
+                ShowAll();
+            }
+        }
+
+        protected void Page_LoadComplete(object sender, EventArgs e)
+        {
+            txtAge.CssClass = "form-control";
+            txtAddress.CssClass = "form-control";
+            txtCity.CssClass = "form-control";
+            txtName.Text = txtAge.Text = txtAddress.Text = txtCity.Text = txtID.Text = "";
+        }
+
+        protected void FillDDL()
+        {
+            ddlCommands.Items.AddRange(new ListItem[] {
+                    new ListItem("INSERT", "Insert"),
+                    new ListItem("UPDATE", "Update"),
+                    new ListItem("DELETE", "Delete"),
+                    new ListItem("FIND", "Find")
+                });
+        }
+
+        protected void ShowAll()
+        {
+            using (SqlConnection connection = new SqlConnection(GetConnection.GetConnectionString()))
+            {
+                SqlCommand cmd = new SqlCommand("Select * from Persons", connection);
+                connection.Open();
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+            }
+        }
+
+        protected void btnExecute_Click(object sender, EventArgs e)
+        {
+            switch (ddlCommands.SelectedValue)
+            {
+                case "Insert":
+                    Insert();
+                    break;
+                case "Update":
+                    Update();
+                    break;
+                case "Delete":
+                    Delete();
+                    break;
+                case "Find":
+                    Find();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        
+
+        protected void ddlCommands_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (ddlCommands.SelectedValue)
+            {
+                case "Insert":
+                    ReqName.Enabled = true;
+                    ReqAge.Enabled = true;
+                    ReqAddress.Enabled = true;
+                    ReqCity.Enabled = true;
+                    ReqID.Enabled = false;
+                    
+                    txtAge.Enabled = true;
+                    txtAddress.Enabled = true;
+                    txtCity.Enabled = true;
+                    txtID.Visible = false;
+                    break;
+                case "Update":
+                    ReqName.Enabled = false;
+                    ReqAge.Enabled = false;
+                    ReqAddress.Enabled = false;
+                    ReqCity.Enabled = false;
+                    ReqID.Enabled = true;
+
+                    txtAge.Enabled = true;
+                    txtAddress.Enabled = true;
+                    txtCity.Enabled = true;
+                    txtID.Visible = true;
+                    break;
+                case "Delete":
+                    ReqName.Enabled = true;
+                    ReqAge.Enabled = false;
+                    ReqAddress.Enabled = false;
+                    ReqCity.Enabled = false;
+                    ReqID.Enabled = false;
+
+                    txtAge.Enabled = false;
+                    txtAddress.Enabled = false;
+                    txtCity.Enabled = false;
+                    txtID.Visible = false;
+                    break;
+                case "Find":
+                    ReqName.Enabled = false;
+                    ReqAge.Enabled = false;
+                    ReqAddress.Enabled = false;
+                    ReqCity.Enabled = false;
+                    ReqID.Enabled = false;
+
+                    txtAge.Enabled = true;
+                    txtAddress.Enabled = true;
+                    txtCity.Enabled = true;
+                    txtID.Visible = false;
+                    break;
+                default:
+                    break;
+            }
+            ShowAll();
+            
+        }
+
+        protected void Insert()
+        {
+            string sqlString = "INSERT INTO Persons (Name, Age, Address, City) VALUES (@Name, @Age, @Address, @City)";
+            using (SqlConnection connection = new SqlConnection(GetConnection.GetConnectionString()))
+            {
+                SqlCommand cmd = GetCommandWithParameters(sqlString, connection);
+                connection.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                lblLog.Text = $"Er zijn {rowsAffected} regels toegevoegd";
+            }
+            ShowAll();
+        }
+
+        protected void Update()
+        {
+            string sqlString = GetSqlVariables();
+            if (sqlString == "")
+            {
+                lblLog.Text = $"Er zijn geen velden aangepast.";
+                ShowAll();
+                return;
+            }
+            sqlString = sqlString.Substring(0, sqlString.Length - 2);
+            sqlString = "UPDATE Persons SET " + sqlString + $" WHERE ID = @ID";
+
+            using (SqlConnection connection = new SqlConnection(GetConnection.GetConnectionString()))
+            {
+                SqlCommand cmd = GetCommandWithParameters(sqlString, connection);
+                connection.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                lblLog.Text = rowsAffected == 0?"Deze ID bestaat niet.":$"Er zijn {rowsAffected} rijen aangepast.";
+            }
+            ShowAll();
+        }
+
+        protected void Delete()
+        {
+            string sqlString = "DELETE FROM Persons WHERE Name = @Name";
+            using (SqlConnection connection = new SqlConnection(GetConnection.GetConnectionString()))
+            {
+                SqlCommand cmd = GetCommandWithParameters(sqlString, connection);
+                connection.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                lblLog.Text = $"Er zijn {rowsAffected} regels gedeletet";
+            }
+            ShowAll();
+        }
+
+        protected void Find()
+        {
+            string sqlString = "SELECT * FROM Persons WHERE Name LIKE @LikeName AND Age = @Age AND Address LIKE @LikeAddress AND City LIKE @LikeCity";
+            string sqlStringNoAge = "SELECT * FROM Persons WHERE Name LIKE @LikeName AND Address LIKE @LikeAddress AND City LIKE @LikeCity";
+            using (SqlConnection connection = new SqlConnection(GetConnection.GetConnectionString()))
+            {
+                SqlCommand cmd;
+                if (txtAge.Text != "")
+                {
+                    cmd = GetCommandWithParameters(sqlString, connection);
+                }
+                else
+                {
+                    cmd = GetCommandWithParameters(sqlStringNoAge, connection);
+                }
+                
+                connection.Open();
+                GridView1.DataSource = cmd.ExecuteReader();
+                GridView1.DataBind();
+                lblLog.Text = $"Er zijn {GridView1.Rows.Count} records gevonden.";
+            }
+        }
+
+        protected string GetSqlVariables()
+        {
+            string sqlString = "";
+            if (txtName.Text != "")
+            {
+                sqlString += $"Name = @Name, ";
+            }
+            if (txtAge.Text != "")
+            {
+                sqlString += $"Age = @Age, ";
+            }
+            if (txtAddress.Text != "")
+            {
+                sqlString += $"Address = @Address, ";
+            }
+            if (txtCity.Text != "")
+            {
+                sqlString += $"City = @City, ";
+            }
+            return sqlString;
+        }
+
+        protected SqlCommand GetCommandWithParameters(string sqlString, SqlConnection connection)
+        {
+            SqlCommand cmd = new SqlCommand(sqlString, connection);
+            if (sqlString.IndexOf("@Name") != -1)
+            {
+                cmd.Parameters.AddWithValue("@Name", txtName.Text);
+            }
+            if (sqlString.IndexOf("@Age") != -1)
+            {
+                cmd.Parameters.AddWithValue("@Age", int.Parse(txtAge.Text));
+            }
+            if (sqlString.IndexOf("@Address") != -1)
+            {
+                cmd.Parameters.AddWithValue("@Address", txtAddress.Text);
+            }
+            if (sqlString.IndexOf("@City") != -1)
+            {
+                cmd.Parameters.AddWithValue("@City", txtCity.Text);
+            }
+            if (sqlString.IndexOf("@ID") != -1)
+            {
+                cmd.Parameters.AddWithValue("@ID", int.Parse(txtID.Text));
+            }
+            if (sqlString.IndexOf("@LikeName") != -1)
+            {
+                cmd.Parameters.AddWithValue("@LikeName", $"%{txtName.Text}%");
+            }
+            if (sqlString.IndexOf("@LikeAddress") != -1)
+            {
+                cmd.Parameters.AddWithValue("@LikeAddress", $"%{txtAddress.Text}%");
+            }
+            if (sqlString.IndexOf("@LikeCity") != -1)
+            {
+                cmd.Parameters.AddWithValue("@LikeCity", $"%{txtCity.Text}%");
+            }
+            return cmd;
+        }
+    }
+}
